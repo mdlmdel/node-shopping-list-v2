26bc5d12e5be3b0903add3945751f3ee9b4ea5b8
mdlmdel <mdlmdel@gmail.com>
2016-12-17 12:32:34 -0500

    Create POST endpoint for /recipes

--- server.js
+++ server.js
@@ -43,10 +43,22 @@ app.post('/shopping-list', jsonParser, (req, res) => {
     }
   }
 
-  const item = ShoppingList.create(req.body.name, req.body.budget);
+// Create POST endpoint for /recipes
+app.post('/recipes', jsonParser, (req, res) => {
+  // ensure `name` and `budget` are in request body
+  const requiredFields = ['name', 'ingredients'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  const item = Recipes.create(req.body.name, req.body.ingredients);
   res.status(201).json(item);
 });
-
 
 app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
